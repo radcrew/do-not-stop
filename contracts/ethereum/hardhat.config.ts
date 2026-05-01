a0022ae93b806054b27de646b03de85b9ccbfe34
Rad Code <betterthanbestest@gmail.com>
2026-05-01 13:17:19 -0400

    config: fix hardhat to listen to ip addresses

--- contracts/ethereum/hardhat.config.ts
+++ contracts/ethereum/hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       type: "http",
-      url: "http://127.0.0.1:8545",
+      url: "http://0.0.0.0:8545",
       chainId: 31337,
       accounts: [
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
